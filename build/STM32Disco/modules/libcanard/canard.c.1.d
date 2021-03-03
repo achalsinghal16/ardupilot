9125ff0a07339807056c0cfddd4e96ca42e8ce4b
alex <achal.johnnette@gmail.com>
2021-03-03 10:55:30 +0530

    STM32Disco Bootloader

---
+++ build/STM32Disco/modules/libcanard/canard.c.1.d
@@ -0,0 +1,4 @@
+modules/libcanard/canard.c.1.o: ../../modules/libcanard/canard.c \
+ ../../modules/libcanard/canard_internals.h \
+ ../../modules/libcanard/canard.h \
+ ../../libraries/AP_Common/missing/string.h
---
+++ build/c4che/_cache.py
@@ -0,0 +1,6 @@
+AUTOCONFIG = True
+BINDIR = '/usr/bin'
+BOARD = 'STM32Disco'
+DEBUG = False
+LIBDIR = '/usr/lib'
+PREFIX = '/usr'
