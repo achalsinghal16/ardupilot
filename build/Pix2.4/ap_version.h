10e6a75e463560f8cba53b638979c6ffbf91f8e6
alex <achal.johnnette@gmail.com>
2021-04-07 17:29:42 +0530

    new pix2.4

--- build/Pix2.4/ap_version.h
+++ build/Pix2.4/ap_version.h
@@ -6,5 +6,5 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "4f426262"
+#define GIT_VERSION "c1762624"
 #define CHIBIOS_GIT_VERSION "331fe75d"
